523fd0f7a491ecce75cfaf7288e89dfa11f7ad2e
MorganQin-Spotec <morganqin-spotec@outlook.com>
2021-04-15 09:59:25 +0800

    1. two sum

---
+++ 1.两数之和.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (var i = 0; i < nums.length - 1; i += 1) {
+    for (var j = i + 1; j < nums.length; j += 1) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return nums;
+};
+// @lc code=end
+
+
+// @after-stub-for-debug-begin
+module.exports = twoSum;
+// @after-stub-for-debug-end
