fe1cbd79f9dde64e37997f5d0c80843ed064089a
MorganQin-Spotec <Acgsior@outlook.com>
2021-04-15 17:55:27 +0800

    Create 27.移除元素.js

---
+++ 27.移除元素.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=javascript
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let index = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] !== val) {
+      nums[index] = nums[i];
+      index += 1;
+    }
+  }
+
+  return index;
+};
+// @lc code=end
+
